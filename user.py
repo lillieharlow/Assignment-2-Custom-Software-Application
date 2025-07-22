8d69028e8da2147de4c343483678dad77231093e
lillieharlow <chapman.lillie@gmail.com>
2025-07-23 08:27:39 +1000

    Added data/players.json. Adding code for new players to sign into game.

--- user.py
+++ user.py
@@ -20,3 +20,26 @@ def load_users():
         except json.JSONDecodeError: # if file is corrupted it starts fresh
             return{}
         
+# save players to file, ensure folder exists
+def save_players(players):
+    os.makedirs("data", exist_ok=True) # if folder exists continue
+    with open(players_file, "w") as f: # open in write mode
+        json.dump(players, f, indent=2) # human readable file storing player data
+        
+# add new player (username & password)
+def add_player():
+    players = load_players()
+    print("\n New Player")
+    while True:
+        playername = input("Please enter your name: ").strip() # .strip() = remove whitespace from start and end of str
+        if not playername:
+            print("Player name can't be empty.")
+            continue
+        if playername in players:
+            print("That player name is already taken.")
+            continue
+        break
+    while True:
+        password = getpass("Password: ").strip()
+        password2 = getpass("Re-enter password: ").strip()
+        
