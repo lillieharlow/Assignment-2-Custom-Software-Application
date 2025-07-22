57dd441bdbd85ed636e1297df8e91f11a38f0aa7
lillieharlow <chapman.lillie@gmail.com>
2025-07-23 06:42:18 +1000

    Added new files/moduels to break down code and make it easier to find, read and write.

---
+++ pet.py
@@ -0,0 +1,11 @@
+class pet:
+    
+    def __init__(self, name, level, points, animal, hungry, happy, clean):
+        self.name = name
+        self.level = level
+        self.points = points
+        self.animal = animal
+        self.hungry = hungry
+        self.happy = happy
+        self.clean = clean
+        
