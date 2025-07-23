bce0ce34701461c1fd6645291b4055c6f7e57724
lillieharlow <chapman.lillie@gmail.com>
2025-07-23 13:58:36 +1000

    Creating tasks.py file code for adding, listing and deleting tasks. Also creating emoji.py to store emoji variables.

--- To-do Manager/tasks.py
+++ To-do Manager/tasks.py
@@ -1,26 +1,25 @@
 '''Functions for task inputs / outputs
 Here you will find the functions for all task input / output'''
 
-
 import emoji
 tasks = []
 
-def addTask():
-  task = input("Please enter a task: ")
+def add_task():
+  task = input("Please enter your new task: ")
   tasks.append(task)
-  print(f"Task '{task}' added to the list.")
+  print(f"TASK: '{task}' WAS added to your list.")
 
 
-def listTasks():
+def list_tasks():
   if not tasks:
-    print("There are no tasks currently.")
+    print("Oh-oh this is awkward... your task list is empty! Let's add a task!")
   else:
     print("Current Tasks:")
     for index, task in enumerate(tasks):
       print(f"Task #{index}. {task}")
 
 
-def deleteTask():
+def delete_task():
   listTasks()
   try:
     taskToDelete = int(input("Enter the # to delete: "))
@@ -31,31 +30,3 @@ def deleteTask():
       print(f"Task #{taskToDelete} was not found.")
   except:
     print("Invalid input.")
-
-
-if __name__ == "__main__":
-  ### Create a loop to run the app
-  print("Welcome to the to do list app :)")
-  while True:
-    print("\n")
-    print("Please select one of the following options")
-    print("------------------------------------------")
-    print("1. Add a new task")
-    print("2. Delete a task")
-    print("3. List tasks")
-    print("4. Quit")
-
-    choice = input("Enter your choice: ")
-
-    if (choice == "1"):
-      addTask()
-    elif (choice == "2"):
-      deleteTask()
-    elif (choice == "3"):
-      listTasks()
-    elif (choice == "4"):
-      break
-    else:
-      print("Invalid input. Please try again.")
-
-  print("Goodbye 👋👋")
