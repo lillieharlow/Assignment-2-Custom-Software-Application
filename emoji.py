aa70a949d458aa585c9527bfe55781223f29b9c9
lillieharlow <chapman.lillie@gmail.com>
2025-07-23 13:45:48 +1000

    To do list! Rer-arranged files, folders. Added task code to tasks.py

---
+++ emoji.py
@@ -0,0 +1,12 @@
+# Custom emoji's as variables to enhance CLI and user visuals
+
+import emoji
+
+# TASK MENU
+emoji_add_task = emoji.emojize(":heavy_plus_sign:")  # ➕
+emoji_delete_task = emoji.emojize(":wastebasket:")   # 🗑️
+emoji_list_task = emoji.emojize(":memo:")            # 📝
+emoji_quit = emoji.emojize(":waving_hand:")          # 👋
+emoji_complete_task = emoji.emojize(":check_mark:")  # ✅
+emoji_edit_task = emoji.emojize(":pencil:")          # ✏️
+emoji_priority_high = emoji.emojize(":exclamation:") # ❗
