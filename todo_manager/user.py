549dd1ad1f3fe1f3e58c484b6e663206b7b83a0f
lillieharlow <chapman.lillie@gmail.com>
2025-07-23 17:06:03 +1000

    hash_password function and verify_password function.

--- todo_manager/user.py
+++ todo_manager/user.py
@@ -4,7 +4,8 @@ Write an AuthManager (auth/auth_manager.py):
 Allow new users to sign up (choose username & password).
 Store hashed passwords securely (Python's hashlib).
 Validate credentials on login.
-Handle errors (e.g., username taken, wrong password).'''
+Handle errors (e.g., username taken, wrong password).
+'''
 
 import hashlib
 import os
@@ -25,3 +26,22 @@ class User:
             self.password_hash = password_hash
         else:
             raise ValueError("Must provide either password or password_hash") # Error handling for developer mistakes
+           
+    '''Hash a password using SHA-256.
+    Plain text passwords are hased to store securely.
+    Takes plin text password and scrambles it into unreadable text
+    hashlib.sha256() = uses SHA-256 algorithm to scramble the password
+    encode() = converts text to bytes (computer language)
+    hexdigest() = converts scrambled bytes back to readable text
+    '''    
+    
+    def _hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+    
+    '''Check if password matches stored hash
+    Compares new scrambled version against the stored scrambled version
+    Boolean functions returns True if match, False if not
+    '''
+
+    def verify_password(self, password):
+        return self.password_hash == self._hash_password(password)
