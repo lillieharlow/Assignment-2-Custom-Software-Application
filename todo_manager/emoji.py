b79191edd1841f57c647c64880266a8b08eba49e
lillieharlow <chapman.lillie@gmail.com>
2025-07-28 09:14:08 +1000

    removed import emoji from venv

--- todo_manager/emoji.py
+++ todo_manager/emoji.py
@@ -1,13 +1,11 @@
-# Custom emoji's as variables to enhance CLI and user visuals
+# Emoji's as variables to enhance CLI and user visuals
 
-import todo_manager.emoji as emoji
-
-emoji_add_task = emoji.emojize(":heavy_plus_sign:")  # ➕
-emoji_delete_task = emoji.emojize(":wastebasket:")   # 🗑️
-emoji_list_task = emoji.emojize(":memo:")            # 📝
-emoji_quit = emoji.emojize(":waving_hand:")          # 👋
-emoji_complete_task = emoji.emojize(":check_mark:")  # ✅
-emoji_edit_task = emoji.emojize(":pencil:")          # ✏️
-emoji_priority_high = emoji.emojize(":exclamation:") # ❗
-emoji_not_found = emoji.emojize(":cross_mark:")  # ❌
-emoji_wink_face = emoji.emojize(":winking_face:")  # 😉
+emoji_add_task = "➕"
+emoji_delete_task = "🗑️"
+emoji_list_task = "📝"
+emoji_quit = "👋"
+emoji_complete_task = "✅"
+emoji_edit_task = "✏️"
+emoji_priority_high = "❗"
+emoji_not_found = "❌"
+emoji_wink_face = "😉"
