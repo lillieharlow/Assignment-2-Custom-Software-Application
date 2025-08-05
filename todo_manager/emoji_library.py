39df06c33e6828602788480ffbf62c5b1b74d222
lillieharlow <chapman.lillie@gmail.com>
2025-08-06 07:24:13 +1000

    Forgot an emoji - missing lock emoji.

--- todo_manager/emoji_library.py
+++ todo_manager/emoji_library.py
@@ -4,20 +4,21 @@
 import emoji
 
 # Main Menu Emojis
-person = emoji.emojize(':bust_in_silhouette:')
-key = emoji.emojize(':key:')
-door = emoji.emojize(':door:')
+person = emoji.emojize(':bust_in_silhouette:')  # 👤
+key = emoji.emojize(':key:')                    # 🔑
+door = emoji.emojize(':door:')                  # 🚪
 
 # Task Menu Emojis
-smile = emoji.emojize(':grinning_face:')
-add = emoji.emojize(':pencil:')
-list = emoji.emojize(':clipboard:')
-complete = emoji.emojize(':check_mark_button:')
-incomplete = emoji.emojize(':white_large_square:')
-delete = emoji.emojize(':wastebasket:')
-quit = emoji.emojize(':waving_hand:')
+smile = emoji.emojize(':grinning_face:')        # 😀
+add = emoji.emojize(':pencil:')                 # ✏️
+list = emoji.emojize(':clipboard:')             # 📋
+complete = emoji.emojize(':check_mark_button:') # ✅
+incomplete = emoji.emojize(':white_large_square:')  # ⬜
+delete = emoji.emojize(':wastebasket:')         # 🗑️
+quit = emoji.emojize(':waving_hand:')           # 👋
 
 # General Emojis
-cross = emoji.emojize(':cross_mark:')
-arm = emoji.emojize(':flexed_biceps:')
-interesting = emoji.emojize(':face_with_monocle:')
+cross = emoji.emojize(':cross_mark:')           # ❌
+arm = emoji.emojize(':flexed_biceps:')          # 💪
+interesting = emoji.emojize(':face_with_monocle:')  # 🧐
+lock = emoji.emojize(':locked:')                # 🔒
